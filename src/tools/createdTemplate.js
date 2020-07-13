db75b1f0c79c6ded3c49a5bd27596d1713fbb99a
chendu <chendu@wepie.com>
2020-07-13 16:13:06 +0800

    [feat] 优化 createdTemplate、getPropsValue

--- src/tools/createdTemplate.js
+++ src/tools/createdTemplate.js
@@ -8,6 +8,12 @@ export function getTemplateValues(name) {
   return window[ANTELOPE_TEMPLATE_STORE][name] || {}
 }
 
-export default function createdTemplate(name, options) {
-  window[ANTELOPE_TEMPLATE_STORE][name] = clone(options)
+export default function createdTemplate(name, options = {}) {
+  if (typeof name === 'string') {
+    window[ANTELOPE_TEMPLATE_STORE][name] = clone(options)
+  } else if (typeof name === 'object') {
+    Object.keys(name).forEach((str) => {
+      window[ANTELOPE_TEMPLATE_STORE][str] = name[str]
+    })
+  }
 }
--- src/utils/getPropsValue.js
+++ src/utils/getPropsValue.js
@@ -1,20 +1,33 @@
-import { getTemplateValues } from '@/tools/createdTemplate'
+import clone from 'lodash/clone'
 
+import { getTemplateValues } from '@/tools/createdTemplate'
 import { quickPorps } from '@/utils/quickPorps'
 
 export default function getPropsValue(vm = {}, keys = []) {
-  let values = [].concat(keys).reduce(
-    (values, key) => {
-      if (typeof vm[key] !== 'undefined') {
-        values[key] = vm[key]
-      } else if (typeof vm[quickPorps[key]] !== 'undefined') {
-        values[key] = vm[quickPorps[key]]
-      }
+  const templateName = vm.template || vm[quickPorps.template]
+  const templateStore = templateName
+    ? clone(getTemplateValues(templateName))
+    : {}
 
-      return values
-    },
-    { ...getTemplateValues(vm.template || vm[quickPorps.template]) }
-  )
+  const values = [].concat(keys).reduce((values, key) => {
+    const quickKey = quickPorps[key] || ''
+
+    if (typeof vm[key] !== 'undefined') {
+      values[key] = vm[key]
+    } else if (typeof vm[quickKey] !== 'undefined') {
+      values[key] = vm[quickKey]
+    }
+
+    if (typeof values[key] === 'undefined') {
+      if (typeof templateStore[key] !== 'undefined') {
+        values[key] = templateStore[key]
+      } else if (typeof templateStore[quickKey] !== 'undefined') {
+        values[key] = templateStore[quickKey]
+      }
+    }
+
+    return values
+  }, {})
 
   return typeof keys === 'string' ? values[keys] : values
 }
